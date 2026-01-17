95bbf6c5e0e49966653a2ac44195a675af4fefb3
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2026-01-17 13:28:54 +0100

    fix - biome fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import "./Footer.css";
 
 export default function Footer() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <footer className="footer">
